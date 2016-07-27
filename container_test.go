7f3ecaa3d1be77821d54ede4af7b7285e8b7418d
Mark DeLillo <mdelillo@pivotal.io>
2016-07-27 10:41:26 -0400

    Error if more than one container is created
    [#113552569]
    
    Signed-off-by: Emily Casey <ecasey@pivotal.io>

--- container_test.go
+++ container_test.go
@@ -1,7 +1,9 @@
 package houdini_test
 
 import (
+	"bytes"
 	"io"
+	"io/ioutil"
 
 	"code.cloudfoundry.org/garden"
 
@@ -19,80 +21,78 @@ var _ = Describe("Container", func() {
 	})
 
 	AfterEach(func() {
-		err := backend.Destroy(container.Handle())
-		Expect(err).ToNot(HaveOccurred())
+		Expect(backend.Destroy(container.Handle())).To(Succeed())
 	})
 
 	Describe("Streaming", func() {
-		Context("between containers", func() {
-			var destinationContainer garden.Container
+		var destinationContainer garden.Container
 
-			BeforeEach(func() {
-				var err error
-				destinationContainer, err = backend.Create(garden.ContainerSpec{})
-				Expect(err).ToNot(HaveOccurred())
-			})
+		AfterEach(func() {
+			Expect(backend.Destroy(destinationContainer.Handle())).To(Succeed())
+		})
 
-			AfterEach(func() {
-				err := backend.Destroy(destinationContainer.Handle())
-				Expect(err).ToNot(HaveOccurred())
-			})
-
-			It("can transfer between containers", func() {
-				process, err := container.Run(garden.ProcessSpec{
-					Path: "sh",
-					Args: []string{
-						"-exc",
-						`
+		It("can stream to and from container", func() {
+			process, err := container.Run(garden.ProcessSpec{
+				Path: "sh",
+				Args: []string{
+					"-exc",
+					`
 							touch a
 							touch b
 							mkdir foo/
 							touch foo/in-foo-a
 							touch foo/in-foo-b
 						`,
-					},
-				}, garden.ProcessIO{
-					Stdout: GinkgoWriter,
-					Stderr: GinkgoWriter,
-				})
-				Expect(err).ToNot(HaveOccurred())
-				Expect(process.Wait()).To(Equal(0))
+				},
+			}, garden.ProcessIO{
+				Stdout: GinkgoWriter,
+				Stderr: GinkgoWriter,
+			})
+			Expect(err).ToNot(HaveOccurred())
+			Expect(process.Wait()).To(Equal(0))
 
-				out, err := container.StreamOut(garden.StreamOutSpec{
-					Path: ".",
-				})
-				Expect(err).ToNot(HaveOccurred())
+			out, err := container.StreamOut(garden.StreamOutSpec{
+				Path: ".",
+			})
+			Expect(err).ToNot(HaveOccurred())
 
-				err = destinationContainer.StreamIn(garden.StreamInSpec{
-					Path:      ".",
-					TarStream: out,
-				})
-				Expect(err).ToNot(HaveOccurred())
+			outBytes, err := ioutil.ReadAll(out)
+			data := ioutil.NopCloser(bytes.NewReader(outBytes))
 
-				nothing := make([]byte, 1)
-				n, err := out.Read(nothing)
-				Expect(n).To(Equal(0))
-				Expect(err).To(Equal(io.EOF))
+			Expect(backend.Destroy(container.Handle())).To(Succeed())
 
-				checkTree, err := destinationContainer.Run(garden.ProcessSpec{
-					Path: "sh",
-					Args: []string{
-						"-exc",
-						`
+			destinationContainer, err = backend.Create(garden.ContainerSpec{})
+			Expect(err).ToNot(HaveOccurred())
+
+			err = destinationContainer.StreamIn(garden.StreamInSpec{
+				Path:      ".",
+				TarStream: data,
+			})
+			Expect(err).ToNot(HaveOccurred())
+
+			nothing := make([]byte, 1)
+			n, err := out.Read(nothing)
+			Expect(n).To(Equal(0))
+			Expect(err).To(Equal(io.EOF))
+
+			checkTree, err := destinationContainer.Run(garden.ProcessSpec{
+				Path: "sh",
+				Args: []string{
+					"-exc",
+					`
 							find .
 							test -e a
 							test -e b
 							test -e foo/in-foo-a
 							test -e foo/in-foo-b
 						`,
-					},
-				}, garden.ProcessIO{
-					Stdout: GinkgoWriter,
-					Stderr: GinkgoWriter,
-				})
-				Expect(err).ToNot(HaveOccurred())
-				Expect(checkTree.Wait()).To(Equal(0))
+				},
+			}, garden.ProcessIO{
+				Stdout: GinkgoWriter,
+				Stderr: GinkgoWriter,
 			})
+			Expect(err).ToNot(HaveOccurred())
+			Expect(checkTree.Wait()).To(Equal(0))
 		})
 	})
 })
